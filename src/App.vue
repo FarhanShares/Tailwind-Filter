3ed2ae2210893a3d32437b6d3f4c567395128bdb
nafeeur10 <nafeeur.kuet.cse6@gmail.com>
2020-11-29 02:58:53 +0600

    Final Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="h-screen bg-gray-200">
+    <CustomFilter />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CustomFilter from './components/CustomFilter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CustomFilter
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
